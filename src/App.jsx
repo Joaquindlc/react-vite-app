539b5b746d5fbed42602e7d65396e851f1ef5b1e
Joaquin <joaquindlc00@gmail.com>
2026-09-16 18:15:22 -0300

    Agregando nombres del grupo Algoritmos para presentacion

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,16 @@ import CharacterList from './Components/CharacterList';
 
 function App() { 
   return (
-  
+  <>
 <div className='bg-dark text-white'>
   <h1 className='text-center display-1 py-4'>Rick and Morty</h1>
+  <p className='text-center py-4'><span>Participantes:</span> Joaquin de la Canal, Luciano Miranda, Ayelen Benitez, Valeria Parodi, Joaquin D´Alessio.</p>
+</div>
+<div className=' text-black'>
   <CharacterList/>
 </div>
+  
+  </>
   )
 }
 
