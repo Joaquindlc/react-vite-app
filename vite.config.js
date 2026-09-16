f10fb42f0e37e40c43a3e5484c99db76d4ba31f4
Joaquin <joaquindlc00@gmail.com>
2026-09-16 17:56:02 -0300

    fix: pages get error, so i remove deploy from a branch to Actions

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://Joaquindlc.github.io/react-vite-app/",
+  base: "react-vite-app",
   plugins: [react()]
 })
